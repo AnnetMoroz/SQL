b15c78b262e868cdc695cca9f3252ec9d41cb1e4
AnnetMoroz <zhitkoanna@mail.ru>
2024-09-02 21:45:20 +0300

    Update Мороз+Анна+Леонидовна_2024-05-30_2288158_SQLiteMoroz.sql
    Выполнить задания:
    1. Напишите запрос, который извлекает все строки из таблицы "Employees".
    2. Найдите всех сотрудников, у которых возраст больше 30 лет.
    3. Найдите всех сотрудников, у которых значение зарплаты равно NULL.
    4. Отсортируйте результат запроса по возрастанию зарплаты.
    5. Выполните запрос, который покажет среднюю зарплату всех сотрудников.
    6. Сгруппируйте данные по отделам и выведите количество сотрудников в
    каждом отделе.
    7. Напишите запрос, который покажет максимальную и минимальную
    зарплату среди всех сотрудников

--- Мороз+Анна+Леонидовна_2024-05-30_2288158_SQLiteMoroz.sql
+++ Мороз+Анна+Леонидовна_2024-05-30_2288158_SQLiteMoroz.sql
@@ -1,4 +1,4 @@
-/*CREATE TABLE Employees (
+CREATE TABLE Employees (
  id INTEGER,
  first_name TEXT,
  last_name TEXT,
@@ -15,17 +15,17 @@ INSERT INTO Employees (id, first_name, last_name, Age, Salary) VALUES
 (7, 'James', 'Anderson', 37, 55000),
 (8, 'Olivia', 'Martinez', 26, 42000),
 (9, 'Daniel', 'Taylor', 45, 70000),
-(10, 'Sophia', 'Garcia', 30, NULL);*/
---SELECT * FROM Employees;
---SELECT first_name,last_name,age FROM Employees WHERE Age>30;
---SELECT first_name,last_name FROM Employees WHERE salary IS NULL;
---SELECT first_name,last_name,salary FROM Employees ORDER BY salary ASC;
---SELECT AVG(Salary) as avg_salary  FROM employees;
-/*ALTER TABLE Employees 
-ADD COLUMN Department TEXT;*/
-/*UPDATE Employees
-SET Department='IT' WHERE id IN (1,4,9,10);*/
-/*UPDATE Employees
-SET Department='HR' WHERE id IN (2,3,5,6,7,8);*/
---SELECT Department,COUNT(*) AS num_employees FROM Employees GROUP BY Department;
+(10, 'Sophia', 'Garcia', 30, NULL);
+SELECT * FROM Employees;
+SELECT first_name,last_name,age FROM Employees WHERE Age>30;
+SELECT first_name,last_name FROM Employees WHERE salary IS NULL;
+SELECT first_name,last_name,salary FROM Employees ORDER BY salary ASC;
+SELECT AVG(Salary) as avg_salary  FROM employees;
+ALTER TABLE Employees 
+ADD COLUMN Department TEXT;
+UPDATE Employees
+SET Department='IT' WHERE id IN (1,4,9,10);
+UPDATE Employees
+SET Department='HR' WHERE id IN (2,3,5,6,7,8);
+SELECT Department,COUNT(*) AS num_employees FROM Employees GROUP BY Department;
 SELECT MAX(Salary) AS max_salary, MIN(Salary) as min_salary FROM Employees;
